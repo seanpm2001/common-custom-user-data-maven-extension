84e451138e499978961113a34d5b806c26116ac7
Jerome Prinet <jprinet@gradle.com>
2022-07-29 17:28:50 +0200

    Configure maven-enforcer-plugin cache rules

--- src/main/java/com/gradle/CustomGradleEnterpriseConfig.java
+++ src/main/java/com/gradle/CustomGradleEnterpriseConfig.java
@@ -3,6 +3,7 @@ package com.gradle;
 import com.gradle.maven.extension.api.GradleEnterpriseApi;
 import com.gradle.maven.extension.api.cache.BuildCacheApi;
 import com.gradle.maven.extension.api.scan.BuildScanApi;
+import com.gradle.maven.extension.api.cache.MojoMetadataProvider;
 
 import java.net.URI;
 
@@ -13,42 +14,98 @@ import java.net.URI;
 final class CustomGradleEnterpriseConfig {
 
     void configureGradleEnterprise(GradleEnterpriseApi gradleEnterprise) {
-        /* Example of Gradle Enterprise configuration
-
-        gradleEnterprise.setServer("https://enterprise-samples.gradle.com");
-        gradleEnterprise.setAllowUntrustedServer(false);
-
-        */
+        gradleEnterprise.setServer("http://localhost:5086");
+        gradleEnterprise.setAllowUntrustedServer(true);
+        //gradleEnterprise.setServer("https://ge-helm-standalone-unstable.grdev.net/");
     }
 
     void configureBuildScanPublishing(BuildScanApi buildScans) {
-        /* Example of build scan publishing configuration
-
         boolean isCiServer = System.getenv().containsKey("CI");
 
         buildScans.publishAlways();
         buildScans.setCaptureGoalInputFiles(true);
         buildScans.setUploadInBackground(!isCiServer);
 
-        */
+        buildScans.tag("api-config");
     }
 
     void configureBuildCache(BuildCacheApi buildCache) {
-        /* Example of build cache configuration
-
-        boolean isCiServer = System.getenv().containsKey("CI");
-
-        // Enable the local build cache for all local and CI builds
-        // For short-lived CI agents, it makes sense to disable the local build cache
         buildCache.getLocal().setEnabled(true);
+        //buildCache.getRemote().getServer().setUrl(URI.create("http://localhost:5086/cache/"));
+        //buildCache.getRemote().getServer().setUrl(URI.create("https://ge.solutions-team.gradle.com/cache/"));
+        buildCache.getRemote().setEnabled(false);
 
-        // Only permit store operations to the remote build cache for CI builds
-        // Local builds will only read from the remote build cache
-        buildCache.getRemote().getServer().setUrl(URI.create("https://enterprise-samples.gradle.com/cache/"));
-        buildCache.getRemote().setEnabled(true);
-        buildCache.getRemote().setStoreEnabled(isCiServer);
-
-        */
+        //TODO test runtime normalization
+        configureBuildCacheAvroPlugin(buildCache);
+        configureBuildCacheEnforcerPlugin(buildCache);
     }
 
+    //TODO
+    void configureBuildCacheAvroPlugin(BuildCacheApi buildCache) {
+
+    }
+
+    void configureBuildCacheEnforcerPlugin(BuildCacheApi buildCache) {
+        buildCache.registerMojoMetadataProvider(context -> {
+            context.withPlugin("maven-enforcer-plugin",() -> {
+                if ("enforce-bytecode-version".equals(context.getMojoExecution().getExecutionId())) {
+                    context.inputs(inputs -> inputs
+                                    .ignore(
+                                            "commandLineRules",
+                                            "fail",
+                                            "failFast",
+                                            "ignoreCache",
+                                            "mojoExecution",
+                                            "session",
+                                            "skip"
+                                    )
+                            )
+                            .nested("project", nestedContext ->
+                                    nestedContext.iterate("artifacts", iteratedContext -> iteratedContext.inputs(nestedInputs -> nestedInputs
+                                            .fileSet("file", fileSet -> fileSet.normalizationStrategy(MojoMetadataProvider.Context.FileSet.NormalizationStrategy.COMPILE_CLASSPATH)
+                                                    .lineEndingHandling(MojoMetadataProvider.Context.FileSet.LineEndingHandling.NORMALIZE))
+                                    ))
+                            )
+                            .iterate("rules", iteratedContext -> iteratedContext
+                                    .inputs(inputs -> inputs.properties("maxJdkVersion", "maxJavaMajorVersionNumber", "ignoredScopes", "ignoreClasses", "ignoreOptionals", "excludes"))
+                            )
+                            .outputs(outputs -> outputs.cacheable("bytecode version check should only run when class files change"));
+                } else if ("enforce-banned-dependencies".equals(context.getMojoExecution().getExecutionId())) {
+                    context.inputs(inputs -> inputs
+                                    .properties(
+                                            "commandLineRules",
+                                            "fail",
+                                            "failFast",
+                                            "ignoreCache",
+                                            "skip"
+                                    )
+                                    .ignore(
+                                            "session",
+                                            "project",
+                                            "mojoExecution"
+                                    )
+                            )
+                            .nested("project", nestedContext ->
+                                    nestedContext.iterate("dependencies", iteratedContext -> iteratedContext.inputs(nestedInputs -> nestedInputs
+                                            .properties("groupId", "artifactId", "version", "type", "scope")
+                                    ))
+                            )
+                            .iterate("rules", iteratedContext -> iteratedContext
+                                    .inputs(inputs -> inputs.properties("includes", "excludes", "searchTransitive", "message"))
+                            )
+                            .outputs(outputs -> outputs.cacheable("banned dependency check should run when dependencies change"));
+                }
+            });
+        });
+    }
+
+    //TODO
+    void configureBuildCacheSpecificKeysInMap(BuildCacheApi buildCache) {
+
+    }
+
+    //TODO
+    void configureBuildCacheSeveralExecutionId(BuildCacheApi buildCache) {
+
+    }
 }
